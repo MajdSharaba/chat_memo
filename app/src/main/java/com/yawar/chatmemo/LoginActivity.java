12fdd80af55ef183951cda3b2803b4bfc385cb96
Majd Sharabah <MajdSharabah8396@gmail.com>
2021-10-24 10:50:47 +0300

    add splash

--- app/src/main/java/com/yawar/chatmemo/LoginActivity.java
+++ app/src/main/java/com/yawar/chatmemo/LoginActivity.java
@@ -2,19 +2,13 @@ package com.yawar.chatmemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_login);
     }
 }
